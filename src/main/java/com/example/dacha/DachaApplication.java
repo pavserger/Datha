bad773aad1c193b6f2a3fd7846dabc83b22b13a9
pavserger@gmai.com <pavserger@gmail.com>
2024-05-30 17:17:18 +0300

    30.05.24 Evening

---
+++ src/main/java/com/example/dacha/DachaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DachaApplication.class, args);
+	}
+
+}
--- src/main/java/controllers/DachaController.java
+++ src/main/java/controllers/DachaController.java
@@ -1,19 +1,56 @@
 package controllers;
 
-import lombok.Data;
+import main.model.Parcel;
 import main.model.ParcelRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
-@Data
-
-
-
 public class DachaController {
 
     private ParcelRepository parcelRepository;
 
+    // Рекомендуемый вариант внедрения зависимости:
+    // внедрение зависимости в класс через конструктор
     public DachaController(ParcelRepository parcelRepository) {
         this.parcelRepository = parcelRepository;
     }
+
+    @GetMapping("/parcels/")
+    public List<Parcel> list() {
+        Iterable<Parcel> parcelIterable = parcelRepository.findAll();
+
+        List<Parcel> parcels = new ArrayList<>();
+        for (Parcel parcel : parcelIterable) {
+            parcels.add(parcel);
+        }
+        return parcels;
+    }
+
+    @PostMapping("/parcels/")
+    public int add(Parcel parcel) {
+        Parcel newParcel = parcelRepository.save(parcel);
+        return newParcel.getId();
+    }
+/*
+    @GetMapping("/books/{id}")
+    public ResponseEntity<?> get(@PathVariable int id) {
+        Optional<Parcel> optionalBook = parcelRepository.findById(id);
+
+        if (!optionalBook.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        return new ResponseEntity<>(optionalBook.get(), HttpStatus.OK);
+    }
+
+ */
 }
